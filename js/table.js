db008e7a07c4f81ff102d2364119fb36f1fe995a
Nrosta <153558060+Astornek@users.noreply.github.com>
2024-02-19 19:27:27 +0100

    Update table.js

--- js/table.js
+++ js/table.js
@@ -2,17 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const year2021Button = document.getElementById("2021");
   const year2022Button = document.getElementById("2022");
   const year2023Button = document.getElementById("2023");
+  const year2024Button = document.getElementById("2024");
 
   const dwaJedenSeason = document.querySelector(".wrapper-jeden");
   const dwaDwaSeason = document.querySelector(".wrapper-dwa");
   const dwaTrzySeason = document.querySelector(".wrapper-trzy");
+  const dwaCzterySeason = document.querySelector(".wrapper-cztery");
+
+  year2024Button.addEventListener("click", function () {
+    dwaDwaSeason.style.display = "none";
+    dwaJedenSeason.style.display = "none";
+    dwaTrzySeason.style.display = "none";
+    dwaCzterySeason.style.display = "block";
+    year2021Button.classList.remove("active");
+    year2022Button.classList.remove("active");
+    year2024Button.classList.add("active");
+    year2023Button.classList.remove("active");
+  });
 
   year2023Button.addEventListener("click", function () {
     dwaDwaSeason.style.display = "none";
     dwaJedenSeason.style.display = "none";
     dwaTrzySeason.style.display = "block";
+    dwaCzterySeason.style.display = "none";
     year2021Button.classList.remove("active");
     year2022Button.classList.remove("active");
+    year2024Button.classList.remove("active");
     year2023Button.classList.add("active");
   });
 
@@ -20,8 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
     dwaDwaSeason.style.display = "block";
     dwaJedenSeason.style.display = "none";
     dwaTrzySeason.style.display = "none";
+    dwaCzterySeason.style.display = "none";
     year2021Button.classList.remove("active");
     year2022Button.classList.add("active");
+    year2024Button.classList.remove("active");
     year2023Button.classList.remove("active");
   });
 
@@ -29,9 +46,11 @@ document.addEventListener("DOMContentLoaded", function () {
     dwaDwaSeason.style.display = "none";
     dwaJedenSeason.style.display = "block";
     dwaTrzySeason.style.display = "none";
+    dwaCzterySeason.style.display = "none";
     year2021Button.classList.add("active");
     year2022Button.classList.remove("active");
     year2023Button.classList.remove("active");
+    year2024Button.classList.remove("active");
   });
 });
 function commingSoon() {
